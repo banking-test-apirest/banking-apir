bf79b20b995e61e880e8388eb0875706414eec80
jaimePalomino <jpalominoc5@upao.edu.pe>
2024-08-24 19:36:31 -0500

    2nd commit

--- bankingservice/src/main/java/com/testbank/bankingservice/model/dto/AccountRequestDTO.java
+++ bankingservice/src/main/java/com/testbank/bankingservice/model/dto/AccountRequestDTO.java
@@ -1,4 +1,28 @@
 package com.testbank.bankingservice.model.dto;
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountRequestDTO {
+    @NotBlank(message = "Account number can not be null")
+    @Size(min = 5, max = 20, message = "The account number must be between 5 and 20 characters")
+    @Pattern(regexp = "[0-9]+", message = "The account number must have only digits")
+    private String accountNumber;
+
+    @NotNull(message = "The balance can not be null")
+    private BigDecimal balance;
+
+    @NotBlank(message = "The owner name can not be null")
+    private String ownerName;
+
+    @NotBlank(message = "The email can not be null")
+    @Email
+    private String ownerEmail;
 }
--- bankingservice/src/main/java/com/testbank/bankingservice/model/entity/Account.java
+++ bankingservice/src/main/java/com/testbank/bankingservice/model/entity/Account.java
@@ -1,4 +1,31 @@
 package com.testbank.bankingservice.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "accounts")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "account_number", nullable = false, unique = true)
+    private String accountNumber;
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+    @Column(name = "owner_name", nullable = false)
+    private String ownerName;
+    @Column(name = "owner_email", nullable = false)
+    private String ownerEmail;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "update_at")
+    private LocalDateTime updatedAt;
 }
